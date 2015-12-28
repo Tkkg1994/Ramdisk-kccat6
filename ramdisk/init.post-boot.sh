5d33754677182850c290248d59293cbbaf101f6a
Tkkg1994 <luca.grifo@outlook.com>
2015-12-28 01:12:10 +0100

    enable new lpm/hpm features in ramdisk

--- ramdisk/init.post-boot.sh
+++ ramdisk/init.post-boot.sh
@@ -61,16 +61,22 @@ if [ -x /system/xbin/busybox ]; then
 fi
 
 #Set CPU Min Frequencies
-echo 268800 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-echo 268800 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-echo 268800 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
-echo 268800 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
+echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 300000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+echo 300000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+echo 300000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
 
 #Set CPU Max Frequencies
-echo 3072000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-echo 3072000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-echo 3072000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
-echo 3072000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+echo 2457600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo 2457600 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo 2457600 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+echo 2457600 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+
+#Set GPU Min Frequencies
+echo 240000000 > /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
+
+#Set GPU Max Frequencies
+echo 600000000 > /sys/class/kgsl/kgsl-3d0/devfreq/max_freq
 
 # Nightmare governor
 chown -R system:system /sys/devices/system/cpu/cpu0/cpufreq/nightmare
